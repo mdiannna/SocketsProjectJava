00d29df0d8f1d198b3352f6b6b3d87b5877e03e2
mdiannna <mdiannna@yahoo.com>
2018-05-23 22:23:30 +0300

    clean code and reorganizing

--- src/client/SocketsClient.java
+++ src/client/SocketsClient.java
@@ -8,60 +8,64 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import players.Cat;
+import server.SocketsServer;
 
+public class SocketsClient extends Frame implements ActionListener {
 
-public class SocketsClient extends Frame implements ActionListener
-{
     private Cat cat;
-    private Label sendLabel;    
+    private Label sendLabel;
     private TextField writeMessage;
-    private TextArea messages; 
+    private TextArea messages;
     private Button sendBtn;
-   
+    private Socket socket;
+    private BufferedReader in = null;
+    private PrintWriter out = null;
+
     public Cat getCat() {
         return this.cat;
     }
-    
+
     public void initLayout() {
         setLayout(new FlowLayout());
-  
+
         messages = new TextArea();
         messages.setEditable(false);       // set to read-only
         add(messages);
 
-        
         sendLabel = new Label("Send");
-        add(sendLabel);    
+        add(sendLabel);
 
-        writeMessage = new TextField("", 20); 
-        add(writeMessage);                    
+        writeMessage = new TextField("", 20);
+        add(writeMessage);
 
         sendBtn = new Button("Send");
         add(sendBtn);
 
         sendBtn.addActionListener(this);
-        setVisible(true);  
-        
-        
+        setVisible(true);
+
         setTitle("Cat game");
-        setSize(700,300);
-        
+        setSize(700, 300);
+
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
     }
-    
-    public SocketsClient(Cat cat)
-    {
-        
+
+    public SocketsClient(Cat cat) {
+
         this.cat = cat;
-        System.out.println("Cat level:"+cat.getLevel());
-        
+        System.out.println("Cat level:" + cat.getLevel());
+
         initLayout();
         System.out.println("I am a " + cat.getColor() + " cat");
         Scanner sc = new Scanner(System.in);
@@ -72,75 +76,100 @@ public class SocketsClient extends Frame implements ActionListener
 
 //        System.out.print("Port server: ");
 //        int portServer = sc.nextInt();
-         int portServer = 9999;
+        int portServer = 9999;
 
-        
         sc.nextLine();
-        
-        //clientul incearca sa se conecteze la server
+
         BufferedReader in;
         PrintWriter out;
-        try
-        {
-            Socket socket = new Socket(adresaServer, portServer);
+        try {
+            this.socket = new Socket(adresaServer, portServer);
 
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream() , true);
+            out = new PrintWriter(socket.getOutputStream(), true);
 
-            while(true)
-            {
+            while (true) {
                 //clientul citeste mesajul de la server
-		String mesajServer = in.readLine();
+                String mesajServer = in.readLine();
 
-                //clientul citeste de la tastatura si transmite server-ului un nume de utilizator 
-                //pana cand numele transmis este diferit de numele tuturor utilizatorilor conectati in acel moment
-                if (mesajServer.startsWith("Nume utilizator?"))
-                {
+                if (mesajServer.startsWith("Nume utilizator?")) {
                     System.out.print("Nume utilizator: ");
                     String nume = sc.nextLine();
                     out.println(nume);
-                } else  if (mesajServer.startsWith("Waiting for matching player..."))
-                    {
-                        System.out.print("Waiting for matching player...");
-                    } else
-                        //server-ul ii transmite clientului faptul ca numele de utilizator a fost acceptat
-                        if (mesajServer.startsWith("Nume acceptat!"))
-                        {
-                            System.out.println("Conectare reusita la server!");
-                            break;
-                        }
+                } else if (mesajServer.startsWith("Waiting for matching player...")) {
+                    System.out.print("Waiting for matching player...");
+                } else //server-ul ii transmite clientului faptul ca numele de utilizator a fost acceptat
+                {
+                    if (mesajServer.startsWith("Nume acceptat!")) {
+                        System.out.println("Conectare reusita la server!");
+                        break;
+                    }
+                }
             }
-			
-            String mesajServer = "";
-			//comunicarea cu server-ul (la fel ca in cazul chat-ului simplu)
 
-            //TODO:threaduri separate pentru trimis si primit mesaje dupa ce s-a conectat? 
-            // TODO: Observer?
-            while (true)
-            {
-                 mesajServer = in.readLine();
+            String mesajServer;
+            while (true) {
+                mesajServer = in.readLine();
 
                 if (mesajServer.startsWith("_CAT_LEVEL?")) {
                     out.println("_CAT_LEVEL:" + cat.getLevel());
-                } else {
-                    System.out.println(mesajServer);
+                    break;
+                }
+            }
 
-                    messages.append(mesajServer + "\n");
-//                    out.println(mesajServer);
-                    System.out.print("Mesaj: ");
-                    String message = sc.nextLine();
-                    out.println(message);
+            MessageThread messageThread = new MessageThread(socket);
 
+            messageThread.addObserver(new Observer() {
+                @Override
+                public void update(Observable obj, Object arg) {
+                    messages.append(arg + "\n");
+                    System.out.println(arg + "\n");
+                }
+            });
+
+            new Thread(messageThread).start();
+
+        } catch (IOException ex) {
+            System.out.println("Conectarea esuata la server: " + ex);
+        } catch (Exception e) {
+            System.out.println("Exception. Somwthing went wrong");
+        }
+    }
+
+    class MessageThread extends Observable implements Runnable {
+
+        private Socket socket;
+
+        public MessageThread(Socket socket) {
+            this.socket = socket;
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+                System.out.println("I am running");
+            } catch (IOException ex) {
+                Logger.getLogger(MessageThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        @Override
+        public void run() {
+            String input = "";
+
+            while (true) {
+                try {
+                    input = in.readLine();
+                    if (input == null) {
+                        break;
+                    }
+                    System.out.println("Input:" + input);
+                    setChanged();
+                    notifyObservers(input);
+                } catch (IOException ex) {
+                    Logger.getLogger(MessageThread.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         }
-        catch (IOException ex)
-        {
-            System.out.println("Conectarea esuata la server: " + ex);
-        }
-        catch(Exception e) {
-            System.out.println("Exception. Somwthing went wrong");
-        }
+
     }
 
     @Override
@@ -148,7 +177,7 @@ public class SocketsClient extends Frame implements ActionListener
         String newMessage = writeMessage.getText();
         writeMessage.setText("");
         System.out.println(">>:" + newMessage + "\n");
-//        messages.setForeground(Color.red);
-        messages.append(newMessage + "\n");
+        out.println(newMessage);
+        messages.append("me:" + newMessage + "\n");
     }
 }
